43a5ea49c916513376787b469e4c603c20456e1d
Diego Uribe <117920815+diegouribe06@users.noreply.github.com>
2022-12-15 20:12:25 -0500

    Local graphic version works. Want to add text inputs with pygame.

--- pygameTests.py
+++ pygameTests.py
@@ -1,18 +1,17 @@
 import copy
-from time import sleep
 import pygame
 from update_stats import update_stats
 
 pygame.init()
 # Alt + Z turns text wrap on and off. Use this to make the comments easier to read since python does not support multi-line comments.
-
 #IDEA
 #Make the circles always there, but black, and get the color values from the moves2 and get_grid functions.
 # Chips will be 100 * 100
 # Lines are currently 20 thick
 player1 = input("Player 1: ")
-player2 = input("Player 2:")
+player2 = input("Player 2: ")
 player_turn = 0
+text = f"{player1}'s Turn"
 
 blank = (0,0,0)
 yellow = (255, 255, 0)
@@ -26,17 +25,21 @@ row_e = {1:blank, 2:blank, 3:blank, 4:blank, 5:blank, 6:blank}
 row_f = {1:blank, 2:blank, 3:blank, 4:blank, 5:blank, 6:blank}
 row_g = {1:blank, 2:blank, 3:blank, 4:blank, 5:blank, 6:blank}
 
+#Scaling
 while True:
     scaling = round(float(input("Enter the scale factor. If the game does not need to be up/downscaled enter 1.")))
     if isinstance(scaling, int):
         break
     else:
-        print("Please enter a valid number.")
+        text = ("Please enter a valid number.")
+
 max_height = 300 * scaling
 max_width = 350 * scaling
 
+font = pygame.font.SysFont("comic_sans_ms", 25 * scaling)
+
 running = True
-screen = pygame.display.set_mode([max_width, max_height])
+screen = pygame.display.set_mode([max_width, max_height + 50 * scaling])
 
 def grid_lines(x1, y1, x2, y2):
     return [screen, (0,0,255), (x1, y1), (x2, y2), 5]
@@ -49,6 +52,7 @@ def get_grid():
 
 def moves2(row):
         global player_turn
+        global text
         rows = get_grid()
         precheck = copy.deepcopy(rows)
 
@@ -56,13 +60,15 @@ def moves2(row):
             #Code to make sure that the player chose a valid spot to go in
             while True:
                 if player_turn == 0:
+                    text = f'{player2}\'s turn'
                     desired_row = row
                 elif player_turn == 1:
+                    text = f'{player1}\'s turn'
                     desired_row = row
                 if (desired_row == 0) or (desired_row == 1) or (desired_row == 2) or (desired_row == 3) or (desired_row == 4) or (desired_row == 5) or (desired_row == 6):
                     break
                 else:
-                    print("That is an invalid selection.")
+                    text = ("Invalid Choice")
             #End of row check section
             #for row in rows:#This loop is to check in every row
             for spot in rows[desired_row]: #For every spot in the row, the algoritm checks if there is anything but an empty chip already occupying the spot.
@@ -84,22 +90,24 @@ def moves2(row):
 
             #Once the change is detected, it breaks out of the while true loop, to make sure that the same player doesn't get locked making all the moves.
             else:
-                print("That row is full. Please choose a different row.")
+                text = "Invalid Choice"
                 break
                 #Finally, no matter the turn, if the algorithm reaches the end, it'll ask the player to choose a different row since the chosen one is full.
 
 def draw_check():
         global row_a, row_b, row_c, row_d, row_e, row_f, row_g, blank
+        global text
         rows = [row_a, row_b, row_c, row_d, row_e, row_f, row_g]
         for row in rows:
             for spot in row:
                 if row[spot] == blank:
                     return False
-        print("It's a tie!")
+        text = "It's a tie!"
         return True
 
 def win_check():
         global player_turn
+        global text
         #Vertical checking from down up, and left to right
         def update_rows(r = True): #Used to make sure that the rows in the function are accurate.
             rows = [row_a, row_b, row_c, row_d, row_e, row_f, row_g]
@@ -115,11 +123,11 @@ def win_check():
         for row in update_rows(): #7 left and right
             for additional in range(3): #2 added for all up and down
                 if vert_check(row, additional) and player_turn == 1: #Player turn has to be reversed for this conditional, because a previous function changes it before the program can check for wins.
-                    print(f'{player1} Wins!')
+                    text = f'{player1} Wins!'
                     update_stats("win")
                     return True
                 elif vert_check(row, additional) and player_turn == 0:
-                    print(f'{player2} Wins!')
+                    text = f'{player2} Wins!'
                     update_stats("lose")
                     return True
         #End of vertical checking
@@ -140,11 +148,11 @@ def win_check():
         for additional in range(4):
             for horizontal in range(1, 7):
                 if horiz_check(additional, horizontal) and player_turn == 1:
-                    print(f'{player1} Wins!')
+                    text = (f'{player1} Wins!')
                     update_stats("win")
                     return True
                 elif horiz_check(additional, horizontal) and player_turn == 0:
-                    print(f'{player2} Wins!')
+                    text = (f'{player2} Wins!')
                     update_stats("lose")
                     return True
         #End of horizontal checking
@@ -157,11 +165,11 @@ def win_check():
         for additional in range(4):
             for horizontal in range(1, 4):
                 if diag_ur_checking(additional, horizontal) and player_turn == 1:
-                    print(f'{player1} Wins!')
+                    text = (f'{player1} Wins!')
                     update_stats("win")
                     return True
                 elif diag_ur_checking(additional, horizontal) and player_turn == 0:
-                    print(f'{player2} Wins!')
+                    text = (f'{player2} Wins!')
                     update_stats("lose")
                     return True
         #End of diagonal up,right checking
@@ -174,15 +182,31 @@ def win_check():
         for additional in range(4):
             for horizontal in range(1, 4):
                 if diag_ul_checking(additional, horizontal) and player_turn == 1:
-                    print(f'{player1} Wins!')
+                    text = (f'{player1} Wins!')
                     update_stats("win")
                     return True
                 elif diag_ul_checking(additional, horizontal) and player_turn == 0:
-                    print(f'{player2} Wins!')
+                    text = (f'{player2} Wins!')
                     update_stats("lose")
                     return True
-
+win = False
 while running:
+    if win:
+        message = font.render(text, True, (255, 255, 255), (0, 0, 0))
+        message_rect = message.get_rect()
+        message_rect.center = (max_width / 2, (max_height + 25) * scaling)
+        screen.blit(message, message_rect)
+        pygame.display.flip()
+        pygame.time.wait(5000)
+        break
+    pygame.display.flip()
+
+    screen.fill((0,0,0)) #Sets the background
+
+    message = font.render(text, True, (255, 255, 255), (0, 0, 0))
+    message_rect = message.get_rect()
+    message_rect.center = (max_width / 2, (max_height + 25) * scaling)
+    screen.blit(message, message_rect)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -191,12 +215,11 @@ while running:
         counter = 0
         for i in range(7):
             if (event.type == pygame.MOUSEBUTTONUP) and (0 + counter < pygame.mouse.get_pos()[0] < (52 * scaling) + counter):
-                #print(f"click in row({i}).")
+                #text = (f"click in row({i}).")
                 moves2(i)
-                #print(get_grid())
+                #text = (get_grid())
             counter += 50 * scaling
 
-    screen.fill((0,0,0)) #Sets the background
 
 
     tracker = 50 * scaling
@@ -214,14 +237,13 @@ while running:
         v_counter = max_height - 25 * scaling
         for v in range(1, 7):
             pygame.draw.circle(*chip(h_counter, v_counter, grid[h][v]))
-            #print(f"{h_counter}, {v_counter}")
+            #text = (f"{h_counter}, {v_counter}")
             v_counter -= 50 * scaling
         h_counter += 50 * scaling
-
-    pygame.display.flip()
     
+
+
+
     if draw_check() or win_check():
-        running = False
-        break
-sleep(5)
+        win = True
 pygame.quit()
